9a194211b4fb410bdd29518eccc0b85458dbc67c
WoolDoughnut310 <wooldoughnutspi@outlook.com>
2022-02-07 06:29:00 +0000

    Updated setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="micropython-firebase-firestore",
+    version="1.0.0",
+    author="WoolDoughnut310",
+    author_email="wooldoughnutspi@outlook.com",
+    description="Firebase Firestore implementation for Micropython optimized for ESP32.",
+    url="https://github.com/WoolDoughnut310/micropython-firebase-firestore",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    packages=["firebase_firestore"]
+)
